193c8dbb3ccba4debc9c77b474b0ae1316d97849
Joshua Vautour <joshua@vautour.org>
2019-02-26 16:49:20 -0500

    Removed common functionality and variables to create a base class.
    Added full docblock compliant comments

--- classes/MicrosoftAPIRequest.php
+++ classes/MicrosoftAPIRequest.php
@@ -1,88 +1,75 @@
 <?PHP
+/**
+ * Class file for MicrosoftAPIRequest
+ * @package 		MicrosoftAPISDK
+ */
 
-/*
- *
+/**
  * Class for connecting to Microsoft API endpoints
  *
  * @author      Joshua Vautour <joshua@speedoflogic.com>
  * @version     0.1.0
+ * @package			MicrosoftAPISDK
  *
  */
+class MicrosoftAPIRequest extends RESTsol{
 
-class MicrosoftAPIRequest {
-	
-	private $microsoftAPIAuthenticationObj = null;
-	private $debugLevelInt = null;
-	private $requestHandle = null;
-
-
-	/*
-	 * Constructor for Microsoft API Request Class
-	 *
-	 * 
+	/**
+	 * A protected varible to that is an instance of {@link MicrosoftAPIAuthentication}
+	 * @var object
 	 */
+	protected $microsoftAPIAuthenticationObj = null;
 
+
+	/**
+	 * Constructor for Microsoft API Request Class
+	 * @param object $microsoftAPIAuthenticationObj an instantiated {@link MicrosoftAPIAuthentication} object
+	 */
 	public function __construct($microsoftAPIAuthenticationObj) {
 		if (!extension_loaded('curl')) {
 			throw new Exception('The PHP exention curl must be installed');
 		}
 		$this->microsoftAPIAuthenticationObj = $microsoftAPIAuthenticationObj;
-		$this->debugLevelInt = $debugLevelInt;
 	}
 
-	private function requestSetup() {
-		$this->requestHandle = curl_init();
-		logInfo("Creating curl handle\n", 5);
-	}
-
-	private function requestDestroy() {
-		curl_close($this->requestHandle); 
-		logInfo("Destroying curl handle\n", 5);
-	}
-
-	private function requestExecute() {
-		logInfo("Executing curl request\n", 5);
-		
-		//	Configure curl option to return transfer data
-		curl_setopt($this->requestHandle, CURLOPT_RETURNTRANSFER, 1);
-
-		$curlResultsMix = curl_exec($this->requestHandle);
-		if(!$curlResultsMix) {
-			logInfo("Curl request failed with message: ".curl_error($this->requestHandle)."\n", 5);
-			throw new Exception('Curl request failed');
-		}
-		logInfo("Results from curl request: ".serialize(curl_getinfo($this->requestHandle))."\n", 9);
-		return $curlResultsMix;
-	}
-
-
+	/**
+	 * Perform a GET request against the Microsoft Graph API
+	 * @param  string $graphRequestStr 	This is data to be appended to the query string
+	 * @param  string $graphVersionStr 	The Graph API version
+	 * @return mixed                  	Contains the return data from the REST request
+	 */
 	public function graphAPIGetRequest($graphRequestStr, $graphVersionStr='v1.0'){
 
-		$graphUrl = 'https://graph.microsoft.com/'.$graphVersionStr.'/'.$graphRequestStr;
-		logInfo("Graph request URL: ".$graphUrl."\n", 9);
+		/**
+		 * Contains the URL for the REST request
+		 * @var string
+		 */
+		$this->requestUrl = 'https://graph.microsoft.com/'.$graphVersionStr.'/'.$graphRequestStr;
+		logInfo("Graph request URL: ".$this->requestUrl."\n", 9);
 
+		/**
+		 * Contains the request method type
+		 * @var string
+		 */
 		$graphRequestMethod = 'GET';
 		logInfo("Graph request Method: ".$graphRequestMethod."\n", 9);
 
+		/**
+		 * build the curl handle
+		 */
 		$this->requestSetup();
 
-		$graphAuthHeaders = $this->microsoftAPIAuthenticationObj->getAuthHeaders();
+		/**
+		 * add the authentication headers to {@link $requestHeaders}
+		 */
+		array_push($this->requestHeaders, $this->microsoftAPIAuthenticationObj->getAuthHeaders());
+		logInfo("Graph request Headers: ".serialize($this->requestHeaders)."\n", 9);
 
-		$graphRequestHeaders = array (
-			"'Accept': 'application/json'",
-			"'Content-Type': 'application/json; charset=utf-8'",
-			"'accept-language': en-US'",
-			"'User-Agent': 'SOLPHPSDK/0.1'",
-			$graphAuthHeaders,
-		);
-		logInfo("Graph request Headers: ".serialize($graphRequestHeaders)."\n", 9);
-
-		// set curl options for the graph request:
-
-		curl_setopt($this->requestHandle, CURLOPT_URL, $graphUrl);
-		curl_setopt($this->requestHandle, CURLOPT_HTTPHEADER, $graphRequestHeaders);
-
-
+		/**
+		 * Set curl URL and header data
+		 */
+		curl_setopt($this->requestHandle, CURLOPT_URL, $this->requestUrl);
+		curl_setopt($this->requestHandle, CURLOPT_HTTPHEADER, $this->requestHeaders);
 
 		// Execute the curl request
 
