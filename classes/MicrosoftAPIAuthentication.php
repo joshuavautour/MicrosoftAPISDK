54d69be186565f9f415b8668a3f7e8ac77f54491
Joshua Vautour <joshua@vautour.org>
2019-02-26 16:50:39 -0500

    Removed common functionality and variables from this class and put them in the base class.
    Added docblock compliant comments to the code.

--- classes/MicrosoftAPIAuthentication.php
+++ classes/MicrosoftAPIAuthentication.php
@@ -1,71 +1,46 @@
 <?PHP
+/**
+ * Class file for MicrosoftAPIAuthentication
+ * @package 		MicrosoftAPISDK
+ */
 
-/*
- *
- * Class for authentication against Microsoft API endpoints
+/**
+ * Class for authenticating against Microsoft APIs
  *
  * @author      Joshua Vautour <joshua@speedoflogic.com>
  * @version     0.1.0
+ * @package			MicrosoftAPISDK
  *
  */
+class MicrosoftAPIAuthentication extends RESTsol {
 
-class MicrosoftAPIAuthentication {
-	
-	private $authTokenStr = null;
-	private $debugLevelInt = null;
-	private $requestHandle = null;
-
-
-	/*
-	 * Constructor for Microsoft API Authentication Class
-	 *
-	 * 
+	/**
+	 * Store the authorization token
+	 * @var string
 	 */
+	protected $authTokenStr = null;
 
-	public function __construct() {
-		if (!extension_loaded('curl')) {
-			throw new Exception('The PHP exention curl must be installed');
-		}
-		$this->microsoftAPIAuthenticationObj = $microsoftAPIAuthenticationObj;
-		$this->debugLevelInt = $debugLevelInt;
-	}
-
-	private function requestSetup() {
-		$this->requestHandle = curl_init();
-		logInfo("Creating curl handle\n", 5);
-	}
-
-	private function requestDestroy() {
-		curl_close($this->requestHandle); 
-		logInfo("Destroying curl handle\n", 5);
-	}
-
-	private function requestExecute() {
-		logInfo("Executing curl request\n", 5);
-		
-		//	Configure curl to return the result
-		curl_setopt($this->requestHandle, CURLOPT_RETURNTRANSFER, 1);
-
-		$curlResultMix = curl_exec($this->requestHandle);
-		if(!$curlResultMix) {
-			logInfo("Curl request failed with message: ".curl_error($this->requestHandle)."\n", 5);
-			throw new Exception('Curl request failed');
-		}
-		logInfo("Curl auth request successful: ".serialize(curl_getinfo($this->requestHandle))."\n", 5);
-		return $curlResultMix;
-	}
-
-
+	/**
+	 * Used to authenticate against the Microsoft Login API using OAuth2
+	 * @param  string $authTenantIdStr        Azure Tenant Id
+	 * @param  string $authClientIdStr        Azure Client Id
+	 * @param  string $authClientSecretStr    Azure Client Secret
+	 * @param  string $authResourceRequestStr Resource being requested with the authentication
+	 * @param  string $authVersionStr         The version of the OAuth2 Login API
+	 * @throws Exception 											Exception is thrown if an unsupported API version is provided
+	 * @return boolean                        Returns true on sucessful login
+	 */
 	public function oauth2UsingProgramSecret($authTenantIdStr, $authClientIdStr, $authClientSecretStr, $authResourceRequestStr, $authVersionStr='v2.0'){
 
 		logInfo("Calling public method oauth2UsingProgramSecret in class MicrosoftAPIAuthentication\n", 9);
 		logInfo("Requestion permission to access: ".$authRequestStr."\n", 9);
 
-		// Build auth token URL
-		$authRequestUrl = 'https://login.microsoftonline.com/'.$authTenantIdStr.'/oauth2/'.$authVersionStr.'/token';
+		$this->requestUrl = 'https://login.microsoftonline.com/'.$authTenantIdStr.'/oauth2/'.$authVersionStr.'/token';
 		logInfo("Auth request URL: ".$authRequestUrl."\n", 5);
 
-		// Build auth request data
+		/**
+		 * At this time only v2.0 authtencitatoion is suppoted
+		 */
 		if ($authVersionStr == 'v2.0') {
 			$authRequestData = 'grant_type=client_credentials&client_id='.$authClientIdStr.'&client_secret='.$authClientSecretStr.'&scope=https%3A%2F%2F'.$authResourceRequestStr.'%2F.default';
 			logInfo("Auth request Data: ".$authRequestData."\n", 5);
@@ -74,63 +49,81 @@ class MicrosoftAPIAuthentication {
 			throw new Exception('Unsupport auth version');
 		}
 
-		// Set Auth request method
-		$authRequestMethod = 'POST';
-
-		$authHeaderData = array (
-			"'Accept': 'application/json'",
-			"'Content-Type': 'application/json; charset=utf-8'",
-			"'accept-language': en-US'",
-			"'User-Agent': 'SOLPHPSDK/0.1'",
-		);
-
-		// Setup the curl request handle
+		/**
+		 * build the curl handle
+		 */
 		$this->requestSetup();
 
+		/**
+		 * add the authentication headers to {@link $requestHeaders}
+		 */
+		logInfo("Graph request Headers: ".serialize($this->requestHeaders)."\n", 9);
 
+		/**
+		 * configure the standard curl options
+		 */
+		$this->requestConfigure();
 
-		logInfo("Auth request Method: ".$authRequestMethod."\n", 5);
-		logInfo("Auth request Headers: ".serialize($authHeaderData)."\n", 5);
-
-		// set curl options for the auth request:
-		curl_setopt($this->requestHandle, CURLOPT_URL, $authRequestUrl);
-		curl_setopt($this->requestHandle, CURLOPT_HTTPHEADER, $authHeaderData);
+		/**
+		 * Set the curl option for the quth request
+		 */
 		curl_setopt($this->requestHandle, CURLOPT_POST, 1);
 		curl_setopt($this->requestHandle, CURLOPT_POSTFIELDS, $authRequestData);
 
-		$authResultsStr = $this->requestExecute();
+		/**
+		 * Execute the curl request
+		 */
+		$this->requestExecute();
 
-		$authResultsArr = json_decode($authResultsStr);
+		/**
+		 * Decode the json results in {@link $requestResult} and save it to an array
+		 * @var array
+		 */
+		$authResultsArr = json_decode($this->requestResult);
 
+		/**
+		 * Set {@link $authTokenStr} from the results of the successful authentication
+		 * @var [type]
+		 */
 		$this->authTokenStr = $authResultsArr->access_token;
 
+		/**
+		 * Destroy the curl handle
+		 */
 		$this->requestDestroy();
 
 		return true;
 	}
 
-	//	Public method to get the current auth token
 
+	/**
+	 * Public method to access the {@link $authTokenStr)
+	 * @throws Exception 		It's trown if authentication has not yet been successfully completed
+	 * @return string 		Microsoft API authorization token
+	 */
 	public function getAuthToken(){
-
 		logInfo("Calling public method getAuthToken in class MicrosoftAPIAuthentication\n", 9);
 
 		if (!$this->authTokenStr) {
 			throw new Exception('Authentication has not been completed');
-		} 
+		}
 		return $this->authTokenStr;
 	}
 
 
-	//	Public method to return the curl auth header 
+	//	Public method to return the curl auth header
 
+	/**
+	 * Public method to get REST authentciation HTTP headers
+	 * @throws Exception 		It's trown if authentication has not yet been successfully completed
+	 * @return string 			The authorization header for a HTTP request
+	 */
 	public function getAuthHeaders(){
-
 		logInfo("Calling public method getAuthHeaders in class MicrosoftAPIAuthentication\n", 9);
 
 		if (!$this->authTokenStr) {
 			throw new Exception('Authentication has not been completed');
-		} 
+		}
 
 		$headerAuth = "Authorization: Bearer ".$this->authTokenStr;
 
